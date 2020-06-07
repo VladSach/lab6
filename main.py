35e6867122860a1381aee76042410d4a2b54ab2d
Vlad <vladsach@gmail.com>
2020-06-07 20:42:13 +0300

    sinus class

--- main.py
+++ main.py
@@ -1,14 +1,35 @@
+import math
+
+
 class Function:
     def __init__(self, variable):
         self.variable = variable
 
-    def count(self, variable):
+    def count(self):
         raise NotImplementedError("Subclass must implement abstract method")
 
 
 class Sin(Function):
-    pass
+
+    def count(self):
+        gradus = self.variable % (360)
+        self.variable = gradus/180*math.pi
+        print(self.variable)
+        i, lasts, s, fact, num, sign = 1, 0, self.variable, 1, self.variable, 1
+        while s != lasts:
+            lasts = s
+            i += 2
+            fact *= i * (i-1)
+            num *= self.variable * self.variable
+            sign *= -1
+            s += num / fact * sign
+            print(s)
+        return +s
 
 
 class Cos(Function):
     pass
+
+
+sinus = Sin(90)
+print(sinus.count())
