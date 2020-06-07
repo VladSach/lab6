e173755eb9c8e21e79e74c9de5bf20cd84e70a37
Vlad <vladsach@gmail.com>
2020-06-07 21:03:49 +0300

    lab6.1 ready

--- lab.6.1/main.py
+++ lab.6.1/main.py
@@ -1,18 +1,24 @@
 import math
-from decimal import *
 
 
 class Function:
+    # Абстрактный класс "функции"
+
     def __init__(self, variable):
         self.variable = variable
 
     def count(self):
+        # Виртуальный метод подсчета функции
         raise NotImplementedError("Subclass must implement abstract method")
 
 
 class Sin(Function):
 
+    def __init__(self):
+        super().__init__(variable)
+
     def count(self):
+        # Переопределненный метод для синуса
         gradus = self.variable % (360)
         self.variable = gradus/180*math.pi
         i, lasts, s, fact, num, sign = 1, 0, self.variable, 1, self.variable, 1
@@ -27,7 +33,11 @@ class Sin(Function):
 
 
 class Cos(Function):
+    def __init__(self):
+        super().__init__(variable)
+
     def count(self):
+        # Переопределненный метод для косинуса
         gradus = self.variable % (360)
         self.variable = gradus/180*math.pi
         i, lasts, fact, num, sign = 0, 0, 1, self.variable, 1
@@ -40,9 +50,3 @@ class Cos(Function):
             sign *= -1
             s += num / fact * sign
         return +s
-
-
-sinus = Sin(90)
-cosinus = Cos(90)
-print(sinus.count())
-print(cosinus.count())
